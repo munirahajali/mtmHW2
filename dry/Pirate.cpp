763d357d892f3771c46e0220cccf33bc55d9c200
97252 <munira.haj@campus.technion.ac.il>
2024-07-15 13:48:11 +0300

    fixed the codes

--- dry/Pirate.cpp
+++ dry/Pirate.cpp
@@ -2,7 +2,7 @@
 #include "Pirate.h"
 
 Pirate::Pirate() {}
-Pirate::Pirate(const string& name): name(name) {}
+Pirate::Pirate(const string& name,int bounty): name(name),bounty(bounty) {}
 
 
 void Pirate::setName(const string& name){
@@ -17,5 +17,13 @@ std::string Pirate::getName(){
 
 std::ostream &operator<<(std::ostream &os, const Pirate &pirate){
     os << pirate.name;
+    os << pirate.bounty;
     return os;
+
+}
+void Pirate::setBounty(int bounty) {
+    this->bounty = bounty;
+}
+int Pirate::getBounty() {
+    return this->bounty;
 }
--- dry/Pirate.h
+++ dry/Pirate.h
@@ -8,16 +8,19 @@ using std::string;
 class Pirate {
 private:
     string name;
+    int bounty;
 
 public:
     Pirate();
-    Pirate(const string& name);
+    Pirate(const string& name,int bounty);
 
     ~Pirate() = default;
 
     void setName(const string& name);
 
     string getName();
+        void setBounty(int bounty);
+        int getBounty();
 
     friend std::ostream &operator<<(std::ostream &os, const Pirate &pirate);
 };
